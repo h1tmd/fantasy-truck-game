65c58872d3f9bf2080a4e4a83d80e6a6c91d0988
h1tmd <105845103+h1tmd@users.noreply.github.com>
2026-01-17 22:37:53 +0800

    Add class name and quest feature

--- actors/player/player.gd
+++ actors/player/player.gd
@@ -1,6 +1,14 @@
+class_name Player
 extends CharacterBody2D
 
 @export var speed: float = 250.0
+
+var current_quest: Quest
+var is_quest_completed = false
+
+func _ready() -> void:
+	current_quest = preload("uid://c1k3rjn6sjhj3")
+	print("Current Quest: " + current_quest.name)
 
 func _physics_process(_delta):
 	var input_dir := Vector2.ZERO
@@ -21,3 +29,17 @@ func _physics_process(_delta):
 	# Rotate car to face movement direction
 	if velocity.length() > 0:
 		rotation = velocity.angle() + PI / 2
+
+func generate_quest(prev_loc:Location):
+	# Generate a random location that is not the previous location
+	pass
+
+func on_location_arrived(location: Location):
+	# Check if location matches with quest
+	if current_quest != null:
+		if current_quest.location == location:
+			is_quest_completed = true
+			print("Quest Completed!")
+			current_quest = null
+			# Generate new quest here
+			generate_quest(location)
