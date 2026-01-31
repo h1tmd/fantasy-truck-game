a32814d961959ff88cf8740182ae3e6726784703
zeekhoft <dltposiedon@gmail.com>
2026-01-31 10:26:42 +0800

    added zombie

--- actors/player/player.gd
+++ actors/player/player.gd
@@ -223,14 +223,14 @@ func player():
 	pass
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.has_method("slime"):
-		enemy_in_range = true
-
-
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.has_method("slime"):
+		enemy_in_range = true
+		
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.has_method("slime"):
 		enemy_in_range = false
+		
 func slime_attack():
 	if enemy_in_range:
 		damage_taken += 1
---
+++ actors/skeleton/skeleton.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+var speed = 50
+var player_chase = false
+var player = null
+
+func _physics_process(_delta):
+	if player_chase:
+		position += (player.position - position)/speed
+		
+		
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	player = body
+	player_chase = true
+		
+		
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	player = null
+	player_chase = false
+
+#func _on_detection_area_body_exited(_body):
+	#player = null
+	#player_chase = false
+	
+func skeleton():
+	pass
